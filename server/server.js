49bf7983cca41a511dc5332c749e3b1d02acc1a5
Jarrod Stone <jarrodstone.92@gmail.com>
2022-05-30 12:57:10 -0400

    Added general error handling

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ app.use(express.json())
 app.use(require('./routes/update.js').router)
 app.use(require('./routes/converter.js').router)
 
+// General error handling
+app.use((err, req, rsp, next) =>  {
+  console.log(err)
+  rsp.status(500).send("500 - Serverside error, please try later")
+})
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on ${PORT}`)
 })
